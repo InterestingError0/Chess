fb434c8312d690343f8fa5ef37b4dff8fcd7b868
InterestingError0 <63620497+InterestingError0@users.noreply.github.com>
2023-09-16 12:44:29 -0400

    General formatting improvements

--- Chess.py
+++ Chess.py
@@ -9,28 +9,30 @@ class Piece:
     
     def draw(self, surface):
         img = pygame.image.load(f"images/{self.colour}_{self.type}.png")
-        surface.blit(img, (self.x*75, self.y*75))
+        surface.blit(img, (self.x * 75, self.y * 75))
 
 pieces = []
 for i in range(8):
     pieces.append(Piece("black", i, 1, "pawn"))
     pieces.append(Piece("white", i, 6, "pawn"))
-pieces.append(Piece("black", 0, 0, "rook"))
+
 pieces.append(Piece("black", 7, 0, "rook"))
 pieces.append(Piece("black", 6, 0, "knight"))
-pieces.append(Piece("black", 1, 0, "knight"))
 pieces.append(Piece("black", 5, 0, "bishop"))
-pieces.append(Piece("black", 2, 0, "bishop"))
-pieces.append(Piece("black", 3, 0, "queen"))
 pieces.append(Piece("black", 4, 0, "king"))
-pieces.append(Piece("white", 0, 7, "rook"))
+pieces.append(Piece("black", 3, 0, "queen"))
+pieces.append(Piece("black", 2, 0, "bishop"))
+pieces.append(Piece("black", 1, 0, "knight"))
+pieces.append(Piece("black", 0, 0, "rook"))
+
 pieces.append(Piece("white", 7, 7, "rook"))
 pieces.append(Piece("white", 6, 7, "knight"))
-pieces.append(Piece("white", 1, 7, "knight"))
 pieces.append(Piece("white", 5, 7, "bishop"))
-pieces.append(Piece("white", 2, 7, "bishop"))
-pieces.append(Piece("white", 3, 7, "queen"))
 pieces.append(Piece("white", 4, 7, "king"))
+pieces.append(Piece("white", 3, 7, "queen"))
+pieces.append(Piece("white", 2, 7, "bishop"))
+pieces.append(Piece("white", 1, 7, "knight"))
+pieces.append(Piece("white", 0, 7, "rook"))
 
 def main():
     pygame.init()
@@ -40,7 +42,7 @@ def main():
     pygame.display.set_caption("Chess")
 
     #Initalize screen with dimensions (640,640)
-    screen = pygame.display.set_mode((640,640))
+    screen = pygame.display.set_mode((640, 640))
 
     #Create surface with dimensions (640,640)
     board = pygame.Surface((600, 600))
@@ -50,8 +52,8 @@ def main():
     #Make every other rectangle light
     for x in range(0, 8, 2):
         for y in range(0, 8, 2):
-            pygame.draw.rect(board, (210, 180, 140), (x*75, y*75, 75, 75))
-            pygame.draw.rect(board, (210, 180, 140), ((x+1)*75, (y+1)*75, 75, 75))
+            pygame.draw.rect(board, (210, 180, 140), (x * 75, y * 75, 75, 75))
+            pygame.draw.rect(board, (210, 180, 140), ((x+1) * 75, (y+1) * 75, 75, 75))
 
     for piece in pieces:
         piece.draw(board)
