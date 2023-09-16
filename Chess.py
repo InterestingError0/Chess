b00833ff964220f8da66103f6b4bc366907d1fb6
InterestingError0 <63620497+InterestingError0@users.noreply.github.com>
2023-09-16 11:55:54 -0400

    Add pawns

--- Chess.py
+++ Chess.py
@@ -1,5 +1,21 @@
 import pygame
 
+class Piece:
+    def __init__(self, colour, x, y, piece_type):
+        self.colour = colour
+        self.x =x
+        self.y = y
+        self.type = piece_type
+    
+    def draw(self, surface):
+        img = pygame.image.load(f"images/{self.colour}_{self.type}.png")
+        surface.blit(img, (self.x*75, self.y*75))
+
+pieces = []
+for i in range(8):
+    pieces.append(Piece("black", i, 1, "pawn"))
+    pieces.append(Piece("white", i, 6, "pawn"))
+
 def main():
     pygame.init()
 
@@ -20,6 +36,9 @@ def main():
         for y in range(0, 8, 2):
             pygame.draw.rect(board, (210, 180, 140), (x*75, y*75, 75, 75))
             pygame.draw.rect(board, (210, 180, 140), ((x+1)*75, (y+1)*75, 75, 75))
+
+    for piece in pieces:
+        piece.draw(board)
 
     #Draws board onto screen, positioned at (20,20)
     screen.blit(board, (20, 20))
